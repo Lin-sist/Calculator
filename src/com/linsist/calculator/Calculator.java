ff5b8d3d1ec693a6b6ab9d00e5e5e41f45c1e2a1
Lin-sist <linitsolong@gmail.com>
2025-04-08 20:07:58 +0800

    增加了package，条理清晰

--- src/com/linsist/calculator/Calculator.java
+++ src/com/linsist/calculator/Calculator.java
@@ -1,35 +1,37 @@
+package com.linsist.calculator;
+
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number1, number2;
-        char operator, choice;
+        String operator = "";
         System.out.println("请输入第一个数字");
         number1 = input.nextInt();
         System.out.println("请输入运算符");
-        operator = input.next().charAt(0);
+        operator = input.next();
         System.out.println("请输入第二个数字");
         number2 = input.nextInt();
 
         switch (operator) {
-            case '+':
-                System.out.println("结果是" + number1 + number2);
+            case "+":
+                System.out.println("结果是" + (number1 + number2));
                 break;
-                case '-':
+                case "-":
                     System.out.println("结果是"+ (number1 - number2));
                     break;
-                    case '*':
+                    case "*":
                         System.out.println("结果是" + number1 * number2);
                         break;
-                        case '/':
+                        case "/":
                             if (number2 == 0) {
                                 System.out.println("除数不能为0，请重新输入非0数！");
                             } else {
                                 System.out.println("结果是" + number1 / number2);
                             }
                             break;
-                            case '%':
+                            case "%":
                                 if (number2 == 0) {
                                     System.out.println("除数不能为0，请重新输入非0数！");
                                 } else {
@@ -39,8 +41,6 @@ public class Calculator {
                             default:
                                 System.out.println("无效的运算符，请重新输入！");
                                 break;
-
-                                
         }
         input.close();
     }
